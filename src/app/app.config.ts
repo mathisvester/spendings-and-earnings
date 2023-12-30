b3c070a8b556e551e592a76e4202838883b62610
Mathis Vester <26164587+mathisvester@users.noreply.github.com>
2023-12-30 14:56:24 +0100

    feat: add @angular/pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { NgxIndexedDBModule } from 'ngx-indexed-db';
 import { dbConfig } from './db-config';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom([NgxIndexedDBModule.forRoot(dbConfig)]),
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 };
