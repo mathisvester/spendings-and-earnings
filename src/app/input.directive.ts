d258904ef6df27fa5500bd54e1de2aa2f9fa51d0
Mathis Vester <26164587+mathisvester@users.noreply.github.com>
2024-01-03 02:07:26 +0100

    feat: adjust input styles

--- src/app/input.directive.ts
+++ src/app/input.directive.ts
@@ -6,5 +6,5 @@ import { Directive, HostBinding } from '@angular/core';
 })
 export class InputDirective {
   @HostBinding('class') class =
-    'block w-full rounded-md border-0 px-2 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600';
+    'block w-full rounded-md border-0 px-2 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 bg-white appearance-none';
 }
